61ffc3d7dc1810d631d385b162534613c9a36dc7
Phantather <akdil.hstech@gmai.com>
2022-02-01 15:07:36 +0600

    new sections

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,11 @@
+
+
+$('ul.works__list').on('click', 'li:not(.works__list-item_active)', function () {
+    $(this)
+        .addClass('works__list-item_active').siblings().removeClass('works__list-item_active')
+        .closest('div.works__tabs').find('div.works__content').removeClass('works__content_active').eq($(this).index()).addClass('works__content_active');
+});
+
 let owl = $('.owl-carousel');
 owl.owlCarousel({
     items:1,
